b508dfdbf6a5a0ea2135c769036d04f598e74b20
mehmeteyupkatirci <mehmeteyupkatirci@gmail.com>
2025-05-08 13:58:59 +0300

    little fix

--- WhatsAppMessageBot/obj/Release/net8.0/win-x64/WhatsAppMessageBot.AssemblyInfo.cs
+++ WhatsAppMessageBot/obj/Release/net8.0/win-x64/WhatsAppMessageBot.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WhatsAppMessageBot")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f981b591bae64bb9b86a8952c900c0e79e58b3e4")]
 [assembly: System.Reflection.AssemblyProductAttribute("WhatsAppMessageBot")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WhatsAppMessageBot")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
